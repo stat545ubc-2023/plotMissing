82b92f9e544d3a1b0a32bec91682fa486135c502
Joey Hotz <johotz37@gmail.com>
2023-11-09 15:16:16 -0800

    Editing the R script, namespace, and description

--- R/plotMissing.R
+++ R/plotMissing.R
@@ -1,19 +1,22 @@
 #' @title Plot counts of missing and recorded values in each selected column
 #'
-#' @details This function returns a stacked column plot which depicts the number of missing (`NA`) and recorded (not `NA`) observations in each specified column of a tibble.
+#' @details This function returns a stacked column plot which depicts the number of missing (`NA`) and recorded (non-`NA`) observations in each specified column of a tibble.
 #'
-#' @param `.data` A tibble in R which contains data. This parameter is named `.data` because it is a generic name for a dataset, and this name matches the convention of other tidyverse functions. The `.` allows this function to be called using a pipe, as opposed to requiring the dataset to be explicitly given in the function call.
-#' @param `...` A collection of additional graphical parameters which will be passed to `ggplot2`. This parameter gives us extra flexibility in creating the plots by dding desired graphical features after the creation of the "basic" plot.
-#' @param `cols` <[`tidy-select`](https://tidyr.tidyverse.org/reference/tidyr_tidy_select.html)> The columns in `.data` which will be shown in the plot. This parameter is named `cols` because it is a selection of columns, and this name matches the convention of other tidyverse functions.
-#' @param `count_names` A vector of two strings, which will be the names given to the counts of missing and recorded values, respectively. This parameter is named `count_names` because the two values in this vector are the names corresponding to the counts of missing and recorded values.
-#' @param `bar_colours` A vector of two colour codes, which are used for depicting the missing and recorded values, respectively. For more information on valid colour codes, see `colors()`. This parameter is named `bar_colours` because that is the proper Canadian spelling, and because the two values in this vector are the colours in the plot which correspond to the missing missing and recorded values.
+#' @param .data A tibble in R which contains data. This parameter is named `.data` because it is a generic name for a dataset, and this name matches the convention of other tidyverse functions. The `.` allows this function to be called using a pipe, as opposed to requiring the dataset to be explicitly given in the function call.
+#' @param ... A collection of additional graphical parameters which will be passed to `ggplot2`. This parameter gives us extra flexibility in creating the plots by dding desired graphical features after the creation of the "basic" plot.
+#' @param cols <[`tidy-select`](https://tidyr.tidyverse.org/reference/tidyr_tidy_select.html)> The columns in `.data` which will be shown in the plot. This parameter is named `cols` because it is a selection of columns, and this name matches the convention of other tidyverse functions.
+#' @param count_names A vector of two strings, which will be the names given to the counts of missing and recorded values, respectively. This parameter is named `count_names` because the two values in this vector are the names corresponding to the counts of missing and recorded values.
+#' @param bar_colours A vector of two colour codes, which are used for depicting the missing and recorded values, respectively. For more information on valid colour codes, see `colors()`. This parameter is named `bar_colours` because that is the proper Canadian spelling, and because the two values in this vector are the colours in the plot which correspond to the missing missing and recorded values.
 #'
-#' @return A stacked column plot displaying the number of missing and recorded observations in each column in `cols`, with the names given in `count_names`, the colours selected in `bar_colours`, and any additional graphical parameters which were provided in `...`.
+#' @return A stacked column plot which displays the number of missing and recorded observations per column in `cols`. This plot has a legend with the group names given in `count_names`, where the colours for each group are provided in `bar_colours`, and the plot also includes any additional graphical parameters which were provided using the `...` argument.
 #' @export
 #'
 #' @examples
-#' library(tidyverse) # this function does not require every single tidyverse library, but it is convenient to load them all
-#' airquality <- tibble::as_tibble(airquality) # load in a built-in dataset in R and convert it to a tibble
+#' # This function does not require every single tidyverse library
+#' # but it is convenient to load them all at once
+#' library(tidyverse)
+#' # load in a built-in dataset in R and convert it to a tibble
+#' airquality <- tibble::as_tibble(airquality)
 #' # displays a plot with the number of missing/recorded observations in each column
 #' plotMissing(airquality)
 #' # subsetting columns, changing the group names, and changing the bar colours
@@ -34,7 +37,7 @@
 plotMissing <- function(.data, ..., cols = everything(),
                         count_names = c("Missing", "Recorded"),
                         bar_colours = c("red", "darkgreen")
-                        ){
+){
   # Check that the provided dataset is a tibble
   if(!tibble::is_tibble(.data)){
     stop(paste("This function requires the provided dataset to be a tibble.",
@@ -84,12 +87,13 @@ plotMissing <- function(.data, ..., cols = everything(),
 
   # Create the column plot to visualize the counts
   missing_data_plot <- data_counts %>%
-    ggplot2::ggplot(aes(x = forcats::fct_rev(variable), y = count, fill = presence)) +
-      ggplot2::geom_col(position = "stack") +
-      ggplot2::scale_fill_manual(values = bar_colours) +
-      ggplot2::labs(x = "Variable", y = "Count", fill = "Presence of Data") +
-      ggplot2::coord_flip() +
-      ggplot_extra_params # Add any extra parameters which were provided
+    ggplot2::ggplot(ggplot2::aes(x = forcats::fct_rev(variable),
+                                 y = count, fill = presence)) +
+    ggplot2::geom_col(position = "stack") +
+    ggplot2::scale_fill_manual(values = bar_colours) +
+    ggplot2::labs(x = "Variable", y = "Count", fill = "Presence of Data") +
+    ggplot2::coord_flip() +
+    ggplot_extra_params # Add any extra parameters which were provided
 
   # Return the plot which was constructed above
   return(missing_data_plot)
